0331e22afa743dbef6c74be0f60ea3357bee41cb
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-03-03 01:20:16 +0100

    add admin model

---
+++ server/Models/Admin.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = Admin = mongoose.model("admins", AdminSchema);
