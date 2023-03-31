503548a199959d52dd6cd82aca9bea20b487137c
moultamissimane <imanelolirock@gmail.com>
2023-03-31 16:17:41 +0100

    edit app.jsx

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Admin from './pages/Admin'
+
+function App() {
+  return (
+    <div>
+     <Admin children={undefined} links={[]} />
+    </div>
+  )
+}
+
+export default App
