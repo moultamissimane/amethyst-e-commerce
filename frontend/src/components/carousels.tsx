aca021b3729edfb00971aab590f5525bb3523cd7
Moultamiss Imane <93978575+moultamissimane@users.noreply.github.com>
2023-05-09 15:00:44 +0000

    remove carousel

--- frontend/src/components/carousels.tsx
+++ frontend/src/components/carousels.tsx
@@ -9,29 +9,39 @@ const Carousels = () => {
   };
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item className='carsouel__item'>
-        <img
-          className='d-block w-full '
-          src='../../public/images/Amethyst.png'
-          alt='First slide'
-        />
-      </Carousel.Item>
-      <Carousel.Item className='carsouel__item'>
-        <img
-          className='img-fluid d-block w-full '
-          src='../../public/images/Am.png'
-          alt='Second slide'
-        />
-      </Carousel.Item>
-      <Carousel.Item className='carsouel__item'>
-        <img
-          className='d-block w-full '
-          src='../../public/images/Amet.jpg'
-          alt='Third slide'
-        />
-      </Carousel.Item>
-    </Carousel>
+    // <Carousel activeIndex={index} onSelect={handleSelect}>
+    //   <Carousel.Item className='carsouel__item'>
+    //     <img
+    //       className='d-block w-full '
+    //       src='../../public/images/Amethyst.png'
+    //       alt='First slide'
+    //     />
+    //   </Carousel.Item>
+    //   <Carousel.Item className='carsouel__item'>
+    //     <img
+    //       className='img-fluid d-block w-full '
+    //       src='../../public/images/Am.png'
+    //       alt='Second slide'
+    //     />
+    //   </Carousel.Item>
+    //   <Carousel.Item className='carsouel__item'>
+    //     <img
+    //       className='d-block w-full '
+    //       src='../../public/images/Amet.jpg'
+    //       alt='Third slide'
+    //     />
+    //   </Carousel.Item>
+    // </Carousel>
+    <div className='container flex justify-between items-center' style={{backgroundColor: "#EEE7DA", }}>
+      <div className=''>
+        <h1>DISCOVER WORLD’S BEST JEWELRY</h1>
+        <p>Our platform provides excellent customer service and fast shipping,
+          making it a reliable choice for customers looking for a hassle-free shopping experience.</p>
+      </div>
+      <div className=''>
+        <img width={400} height={400} src="../public/images/home-image.png" alt="" />
+      </div>
+    </div>
   );
 };
 
