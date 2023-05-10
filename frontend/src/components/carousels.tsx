5c7aaf6160b2810fe05fcd4a78a768a37ad3681a
Moultamiss Imane <93978575+moultamissimane@users.noreply.github.com>
2023-05-10 22:49:47 +0000

    edit carousel section

--- frontend/src/components/carousels.tsx
+++ frontend/src/components/carousels.tsx
@@ -37,14 +37,14 @@ const Carousels = () => {
     // </Carousel>
     <div className='' style={{ backgroundColor: "#EEE7DA", }}>
 
-    <div className='container row justify-content-between ' >
+    <div className='row justify-content-between ' >
       <div className='col-lg-4 col-md-5 col-12 mr-9'>
         <h1 className=''>DISCOVER WORLD’S BEST JEWELRY</h1>
         <p>Our platform provides excellent customer service and fast shipping,
           making it a reliable choice for customers looking for a hassle-free shopping experience.</p>
       </div>
-      <div className='col-lg-8 col-md-7'>
-        <img width={400} height={400} src="../public/images/home-image.png" alt="" />
+      <div className='d-flex col-lg-8 col-md-7 mt-20 justify-content-end'>
+        <img width={400} height={400} src="/images/home-image.png" alt="" />
       </div>
     </div>
     </div>
